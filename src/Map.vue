d812b1a7dc98567965f1ca1d38d3cadf3552b651
PhamPhuTuan <phamphutuan0710pt@gmail.com>
2023-02-09 07:21:28 +0700

    .

--- src/Map.vue
+++ src/Map.vue
@@ -29,13 +29,13 @@
           .setLngLat([144.964635,-37.816938])
           .addTo(map.value);
         new Marker({color: "#FF0000"})
-          .setLngLat([145.013687,-37.810907])
+          .setLngLat([144.960814,-37.816874])
           .addTo(map.value);
         new Marker({color: "#FF0000"})
-          .setLngLat([144.934861,-37.812113])
+          .setLngLat([144.969441,-37.814156])
           .addTo(map.value);
         new Marker({color: "#FF0000"})
-          .setLngLat([144.959482,-37.849047])
+          .setLngLat([144.960455,-37.813426])
           .addTo(map.value);
         new Marker({color: "#FF0000"})
           .setLngLat([144.992883,-37.847992])
